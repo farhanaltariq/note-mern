00d3fff4f548c9f9302ae930d104c5316bd0b65f
Farhan Altariq <farhanaaxy@gmail.com>
2022-06-16 16:59:06 +0700

    Update styling, progress on edit function

--- frontend/src/todo-create/ToDoCreate.js
+++ frontend/src/todo-create/ToDoCreate.js
@@ -1,17 +1,20 @@
 import "./ToDoCreate.css";
 import { useState } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const ToDoCreate = (props) => {
     const [getNewTodo, setNewTodo] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const newTodo = {
-            // id: Math.floor(Math.random() * 50000) + 1,
-            title: getNewTodo,
-        };
-        axios.post("http://localhost:5000/", newTodo).then((res) => {});
+        if (getNewTodo.length > 0) {
+            const newTodo = {
+                title: getNewTodo,
+            };
+            axios.post("http://localhost:5000/", newTodo).then((res) => {});
+        }
         setNewTodo(``);
     };
     const handleNewTodo = (e) => {
@@ -20,23 +23,33 @@ const ToDoCreate = (props) => {
     props.func();
 
     return (
-        <>
-            <form className="todo-form row g-3" onSubmit={handleSubmit}>
-                <div className="col-auto">
-                    <input
-                        className="form-control"
-                        type="text"
-                        value={getNewTodo}
-                        onChange={handleNewTodo}
-                    />
-                </div>
-                <div className="col-auto">
-                    <button className="btn btn-primary mb-2" type="submit">
-                        Add
-                    </button>
-                </div>
-            </form>
-        </>
+        <form
+            className="text-center"
+            style={{ width: "80%", marginInlineStart: "10%" }}
+            onSubmit={handleSubmit}
+        >
+            <div className="input-group mb-3">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="New Note"
+                    aria-describedby="basic-addon2"
+                    value={getNewTodo}
+                    onChange={handleNewTodo}
+                />
+                <button
+                    type="submit"
+                    className="input-group-text text-white bg-primary"
+                    id="basic-addon2"
+                >
+                    Add
+                    <FontAwesomeIcon
+                        className="text-white"
+                        icon={faPlus}
+                    ></FontAwesomeIcon>
+                </button>
+            </div>
+        </form>
     );
 };
 export default ToDoCreate;
--- frontend/src/todo-list/ToDoList.js
+++ frontend/src/todo-list/ToDoList.js
@@ -1,24 +1,51 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faPen } from "@fortawesome/free-solid-svg-icons";
+import {
+    faTrash,
+    faPen,
+    faFloppyDisk,
+} from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 import "./ToDoList.css";
 
+let clicked = false;
 const ToDoList = (props) => {
     const destroy = (id) => {
         axios.delete(`http://localhost:5000/${id}`).then((res) => {});
+    };
+    const changeElement = (id) => {
+        const data = document.getElementById(id);
+        if (!clicked) {
+            data.outerHTML = `<input type="text" class="col form-control" id="${id}" value="${data.innerText}"/>`;
+            clicked = true;
+        } else {
+            data.outerHTML = `<div class="col" id="${id}">${data.value}</div>`;
+            clicked = false;
+        }
     };
     return (
         <ul className="list-group">
             {props.dataTodos.map((todo) => {
                 return (
-                    <li className="list-group-item" key={todo._id}>
+                    <li
+                        className="list-group-item py-auto my-2 pt-2"
+                        key={todo._id}
+                    >
                         <div className="row">
-                            <div className="col">{todo.title}</div>
-                            <div className="col btn text-end">
-                                <button className="btn">
+                            <div className="col" id={todo._id}>
+                                {todo.title}
+                            </div>
+                            <div className="col text-end">
+                                <button
+                                    className="btn"
+                                    onClick={() => changeElement(todo._id)}
+                                >
                                     <FontAwesomeIcon
-                                        className="text-info"
-                                        icon={faPen}
+                                        className={
+                                            clicked
+                                                ? "text-success"
+                                                : "text-info"
+                                        }
+                                        icon={clicked ? faFloppyDisk : faPen}
                                     ></FontAwesomeIcon>
                                 </button>
                                 <button
